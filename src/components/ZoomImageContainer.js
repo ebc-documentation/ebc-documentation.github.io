9a7dcad23299296cc2fd9e860befd669894060fb
Jirapus Eiamsomboon <jirapus@blockfint.com>
2022-01-19 17:44:33 +0700

    feat: Add zoom in button

--- src/components/ZoomImageContainer.js
+++ src/components/ZoomImageContainer.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import Zoom from 'react-medium-image-zoom';
+import React, { useCallback, useState, useRef } from 'react';
+import styled from 'styled-components';
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
+import ZoomInIcon from '@site/static/img/zoom_in.svg';
 
-export default function ZoomImageContainer({ img, alt = '', children }) {
+const StyledControlledZoomContainer = styled.div`
+  position: relative;
+  display: flex;
+
+  & > div > button {
+    display: none;
+  }
+`;
+
+const StyledZoomIconContainer = styled.div`
+  position: absolute;
+  width: 1.65rem;
+  top: auto;
+  left: auto;
+  bottom: 0;
+  right: -3rem;
+  cursor: pointer;
+`;
+
+export default function ZoomImageContainer({ img, alt = '' }) {
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  const handleZoom = () => {
+    setIsZoomed((prev) => {
+      return !prev;
+    });
+  };
+
+  const handleZoomChange = useCallback((shouldZoom) => {
+    setIsZoomed(shouldZoom);
+  }, []);
+
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-      {/* <Zoom>{img ? <img alt={alt} src={img} /> : children}</Zoom> */}
-      {img ? <img alt={alt} src={img} /> : children}
-    </div>
+    <StyledControlledZoomContainer>
+      <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
+        <img alt={alt} src={img} />
+      </ControlledZoom>
+      <StyledZoomIconContainer
+        id="zoom-icon"
+        isZoomed={isZoomed}
+        onClick={handleZoom}
+      >
+        <ZoomInIcon />
+      </StyledZoomIconContainer>
+    </StyledControlledZoomContainer>
   );
 }
