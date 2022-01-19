ffe2afbde57633f3d4c95bf6947f287120448591
Jirapus Eiamsomboon <jirapus@blockfint.com>
2022-01-19 17:04:04 +0700

    feat: Disable zoom image

--- src/components/ZoomImageContainer.js
+++ src/components/ZoomImageContainer.js
@@ -5,7 +5,8 @@ import 'react-medium-image-zoom/dist/styles.css';
 export default function ZoomImageContainer({ img, alt = '', children }) {
   return (
     <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-      <Zoom>{img ? <img alt={alt} src={img} /> : children}</Zoom>
+      {/* <Zoom>{img ? <img alt={alt} src={img} /> : children}</Zoom> */}
+      {img ? <img alt={alt} src={img} /> : children}
     </div>
   );
 }
