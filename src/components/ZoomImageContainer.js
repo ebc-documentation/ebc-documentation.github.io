8974d5c9bd44f608b3cf2b7a30be57052738720d
Jirapus Eiamsomboon <jirapus@blockfint.com>
2022-01-19 15:26:05 +0700

    Add more content to technical overview page

--- src/components/ZoomImageContainer.js
+++ src/components/ZoomImageContainer.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-export default function ZoomImageContainer({ img, alt = '' }) {
+export default function ZoomImageContainer({ img, alt = '', children }) {
   return (
     <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-      <Zoom>
-        <img alt={alt} src={img} />
-      </Zoom>
+      <Zoom>{img ? <img alt={alt} src={img} /> : children}</Zoom>
     </div>
   );
 }
